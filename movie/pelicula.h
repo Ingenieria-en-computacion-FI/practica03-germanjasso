6024322be0108389b204c96ba8a0f5aec22ba2da
germanjasso <germanjasso610@gmail.com>
2026-03-08 20:26:38 -0600

    Update pelicula.h

--- movie/pelicula.h
+++ movie/pelicula.h
@@ -3,22 +3,16 @@
 
 #define MAX_DIRECTORES 10
 
-/* Tipo opaco */
 typedef struct Pelicula Pelicula;
 
-/* Crear una película */
-Pelicula* crearPelicula(const char* titulo, int anio, const char* genero);
+Pelicula* crearPelicula(const char* titulo, int año, const char* genero);
 
-/* Imprimir información de la película */
 void imprimir(Pelicula* p);
 
-/* Cambiar el género */
 void cambiarGenero(Pelicula* p, const char* nuevoGenero);
 
-/* Agregar un director */
 void agregarDirector(Pelicula* p, const char* director);
 
-/* Liberar memoria */
 void destruir(Pelicula* p);
 
 #endif
